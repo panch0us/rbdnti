b9bd035a81251e2ba10afe044ec981f92caf0212
panch0us <godbryansk@yandex.ru>
2025-11-11 12:26:14 +0300

    rebuild

--- rbdnti/news_site/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class MultiFileUploadForm(forms.Form):
-    news_id = forms.IntegerField(widget=forms.HiddenInput())
-    # Убираем поле files полностью - будем обрабатывать напрямую в view
